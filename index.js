1f0789cf80e960df766512f132122f50e33d8e3a
RafiKastner <rafikastner@gmail.com>
2024-04-10 16:16:33 -0400

    Tomorrow.io for weather data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Datastore = require('nedb');
+const fetch = require('node-fetch');
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
@@ -26,4 +27,16 @@ app.post('/api', (request, response) => {
         latitude: data.lat,
         longitude : data.lon,
     })
+});
+
+app.get('/weather/:latlon', async (request, response) => {
+    console.log(request)
+    const latlon = request.params.latlon.split(',');
+    const lat = latlon[0];
+    const lon = latlon[1];
+    console.log(lat,lon);
+    const api_url = `https://api.tomorrow.io/v4/weather/forecast?location=${lat},${lon}&apikey=R0w2hiuMUFGLlOlKPxiNnf6b45UahENy`
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    response.json(json);
 });
--- public/main.js
+++ public/main.js
@@ -1,27 +1,30 @@
 const page = "/logs/index.html";
 const submit = document.getElementById("submit");
-const globals = {}
 
 if ("geolocation" in navigator) {
-        console.log("geolocation available")
-        position();
-} else {
-        console.log("geolocation not available");
-}
-
-submit.addEventListener('click', async event => {
-    position().then(({lat, lon}) => {
-        data(lat, lon);
+    console.log("geolocation available")
+    position();
+    submit.addEventListener('click', async event => {
+        position().then(({lat, lon}) => {
+            data(lat, lon);
+        })
     })
-})
+} else {
+    console.log("geolocation not available");
+}
 
 function position() {
     return new Promise((resolve) => {
-        navigator.geolocation.getCurrentPosition(position => {
+        navigator.geolocation.getCurrentPosition(async position => {
             const lat = position.coords.latitude
             const lon = position.coords.longitude
             document.getElementById("latitude").textContent = lat;
             document.getElementById("longitude").textContent = lon;
+            const api_url = `/weather/${lat},${lon}`;
+            //&apikey=R0w2hiuMUFGLlOlKPxiNnf6b45UahENy
+            const response = await fetch(api_url);
+            const json = await response.json();
+            console.log(json)
             resolve({lat, lon});
         })
     })
