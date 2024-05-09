433972098b828a11128e791ef3b3e794e115fccd
RafiKastner <rafikastner@gmail.com>
2024-05-08 20:59:13 -0400

    e

--- back-end/index.js
+++ back-end/index.js
@@ -14,7 +14,7 @@ const database = new Datastore('database.db');
 database.loadDatabase();
 
 app.get('/', function (request, response) {
-    response.sendFile(path.join(_durname + "index.html"))
+    response.sendFile("index.html")
 });
 
 app.get('/api', (request, response) => {
