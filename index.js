6f27bea442c083fc71f9512491836916b27f4029
RafiKastner <rafikastner@gmail.com>
2024-05-11 11:14:59 -0400

    port bug testing

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var corsOptions = {
     origin: 'http://example.com',
     optionSuccessStatus: 200
 }
-console.log(port)
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '1mb' }));
 
@@ -22,6 +21,7 @@ database.loadDatabase();
 
 app.get('/api', (request, response) => {
     database.find({}, (err, data) => {
+        data.port = port;
         response.json(data);
     })
 });
