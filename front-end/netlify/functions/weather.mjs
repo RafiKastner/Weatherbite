16ee520c1e8cc9505220b4ff4cd4167c4e77ced2
RafiKastner <rafikastner@gmail.com>
2024-05-31 16:29:58 -0400

    new .env key name

--- front-end/netlify/functions/weather.mjs
+++ front-end/netlify/functions/weather.mjs
@@ -2,7 +2,7 @@ export default async (event, context) => {
     try {
         const lat = context.geo.latitude;
         const lon = context.geo.longitude;
-        const api_url = `https://api.tomorrow.io/v4/weather/forecast?location=${lat},${lon}&apikey=${process.env.API_KEY}`
+        const api_url = `https://api.tomorrow.io/v4/weather/forecast?location=${lat},${lon}&apikey=${process.env.TOMORROW_IO_KEY}`
         const fetch_response = await fetch(api_url);
         const json = await fetch_response.json();
         console.log(json)
